df05798e8c3830e135f1705c40435785904cd08d
konstantin.kuznetcov <konstantin.kuznetcov@dowjones.com>
2022-09-29 16:40:20 -0400

    action, actionType, enum, action creator

---
+++ src/state/action-creator/index.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+export const searchRepositories = (term: string) => {
+  return async (dispatch: any) => {
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES,
+    });
+
+    try {
+      const { data } = await axios.get(
+        "https://registry.npmjs.org/-/v1/search",
+        {
+          params: {
+            text: term,
+          },
+        }
+      );
+
+      const result = data.objects.map((obj: any) => {
+        return obj.package.name;
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        dispatch({
+          type: ActionType.SEARCH_REPOSITORIES_ERROR,
+          payload: error.message,
+        });
+      }
+    }
+  };
+};
---
+++ src/state/action-types/index.ts
@@ -0,0 +1,5 @@
+export enum ActionType {
+  SEARCH_REPOSITORIES = "rearch_repositories",
+  SEARCH_REPOSITORIES_SUCCESS = "rearch_repositories_success",
+  SEARCH_REPOSITORIES_ERROR = "rearch_repositories_error",
+}
